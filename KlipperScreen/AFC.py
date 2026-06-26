fad17607f99a88022f482f0107edb22f55383e9f
cdheiser <10488026+cdheiser@users.noreply.github.com>
2026-06-25 19:37:28 -0700

    Rename apilayer from apiclent to restApi
    The apilayer recently changed from apiclient to restApi.

--- KlipperScreen/AFC.py
+++ KlipperScreen/AFC.py
@@ -211,7 +211,7 @@ class Panel(ScreenPanel):
     def __init__(self, screen, title):
         title = title or ("AFC Status")
         super().__init__(screen, title)
-        self.apiClient = screen.apiclient
+        self.apiClient = screen.restApi
         self.lane_widgets = {}
         AFClane.theme_path = screen.theme
         logging.info(f"Theme path: {AFClane.theme_path}")
